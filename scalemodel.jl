ca8e22304f03ae4133459fb3f57a1a9de502bf4d
kjl <keithjl@mit.edu>
2023-02-25 15:40:35 -0500

    .

--- scalemodel.jl
+++ scalemodel.jl
@@ -8,6 +8,7 @@ begin
     height = 229. #mm
     e = height #eccentricity of external strands from centroid 
     Lseg = 500. #mm
+    Ldev = 89. #mm 
 
     fc = 30. #MPa, N/mm²
 
@@ -28,13 +29,13 @@ end
 begin
     Splate = b * tPlate^2 / 6 #section modulus, mm³
     Mrplate = Splate * fySteel #yielding moment, Nmm
-    Frod = Mrplate / momentArm #Force at anchor, N
+    FrodHorizontal = Mrplate / momentArm #Force at anchor, N
 end
 
 ## Global demand
 #Angle of strand + force in horizontal strand region [rad, N]
 θ = atan(e/Lseg)
-FrodHorizontal = Frod * cos(θ)
+Frod = FrodHorizontal / cos(θ)
 
 #Required external load [N]
 Preq = 2Frod * sin(θ)
@@ -54,3 +55,13 @@ RsteelAxial = axialFactor * FuAxial * 3 #total post tension force on concrete [N
 Rtotal = 0.65 * 0.8 * (Rconcrete - RsteelAxial) #total factored resistance [N]
 
 ##Deviator design
+### failure mode 1: slip critical
+tensioning = collect(range(0., 2Frod, length = 300))
+Fvert = tensioning .* sin(θ)
+Fhor = tensioning .* cos(θ)
+
+prop = collect(0:0.05:0.2)
+μs = collect(0.4:0.05:0.7)
+
+using GLMakie, kjlMakie
+
