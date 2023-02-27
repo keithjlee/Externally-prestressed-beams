b16642c01330199e13809f4dff0fea7e6365bb71
kjl <keithjl@mit.edu>
2023-02-27 12:43:43 -0500

    loading beam analysis

--- scalemodel.jl
+++ scalemodel.jl
@@ -55,6 +55,12 @@ RsteelAxial = axialFactor * FuAxial * 3 #total post tension force on concrete [N
 Rtotal = 0.65 * 0.8 * (Rconcrete - RsteelAxial) #total factored resistance [N]
 
 ##Deviator design
+hdev = 87.2
+wdev = 50.8
+
+Moverturn = 2Frod * cos(θ) * hdev / 2
+Poverturn = Moverturn * 6 / wdev
+
 ### failure mode 1: slip critical
 tensioning = collect(range(0., 2Frod, length = 300))
 Fvert = tensioning .* sin(θ)
@@ -63,5 +69,48 @@ Fhor = tensioning .* cos(θ)
 prop = collect(0:0.05:0.2)
 μs = collect(0.4:0.05:0.7)
 
+Ffriction = [f * μ for f in Fvert, μ in μs]
+
 using GLMakie, kjlMakie
 
+set_theme!(kjl_light)
+begin
+    fig = Figure(backgroundcolor = :white)
+
+    ax = Axis3(fig[1,1],
+        xlabel = "Induced normal force [N]",
+        ylabel = "Friction coefficient, μ",
+        zlabel = "Horizontal friction resistance [N]",
+        zlabeloffset = 75,
+        aspect = (1,1,1))
+
+    labelize!(ax)
+    surface!(Fvert, μs, Ffriction,
+        colormap = :tempo)
+
+    fig
+end
+
+
+### Aluminum loading plate
+hplate = 7 * 25.4 
+bplate = 4.5 * 25.4 
+tflange = 3/8 * 25.4
+tweb = 25.4 / 4
+
+Iflange = bplate * tflange^3 / 12
+Aflange = bplate * tflange
+rflange = hplate / 2 - tflange / 2
+
+Iweb = tweb * (hplate - 2tflange)^3 / 12
+
+Itot = 2 * (Iflange + Aflange * rflange^2) + Iweb #mm^4
+Eal = 70e3 #N/mm²
+σal = 95e3 #N/mm²
+
+Stot = Itot / hplate * 2
+
+Pcenter = 6e3 #N
+Lplate = 1000. #mm
+
+Δmax = Pcenter * Lplate^3 / 48 / Eal / Itot
